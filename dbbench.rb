429791bfe16982c309b8086d8a0282755cdb974b
Simon Jürgensmeyer <Simon.Juergensmeyer@gmail.com>
2018-12-16 10:52:02 +0100

    dbbench v0.6.0

--- dbbench.rb
+++ dbbench.rb
@@ -1,9 +1,9 @@
 class Dbbench < Formula
   desc "dbbench is a simple database benchmarking tool which supports several databases"
   homepage ""
-  url "https://github.com/sj14/dbbench/releases/download/v0.5.2/dbbench_0.5.2_darwin_amd64.tar.gz"
-  version "0.5.2"
-  sha256 "aad80a9a61d4120675581e3d303b631f947e90a15012964255ca46f075a6352d"
+  url "https://github.com/sj14/dbbench/releases/download/v0.6.0/dbbench_0.6.0_darwin_amd64.tar.gz"
+  version "0.6.0"
+  sha256 "2b8f1d2879a908b7ef6cea031b64a0ad029a0de05fa20612c44718e81094c6e0"
 
   def install
     bin.install "dbbench"
