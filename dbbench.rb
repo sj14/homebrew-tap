150f146f5be4cd081f87716da1235836d808d639
Simon Jürgensmeyer <Simon.Juergensmeyer@gmail.com>
2018-12-22 14:05:38 +0100

    Removed subfolder

--- dbbench.rb
+++ dbbench.rb
@@ -1,9 +1,9 @@
 class Dbbench < Formula
   desc "dbbench is a simple database benchmarking tool which supports several databases"
   homepage ""
-  url "https://github.com/sj14/dbbench/releases/download/v0.6.0/dbbench_0.6.0_darwin_amd64.tar.gz"
-  version "0.6.0"
-  sha256 "2b8f1d2879a908b7ef6cea031b64a0ad029a0de05fa20612c44718e81094c6e0"
+  url "https://github.com/sj14/dbbench/releases/download/v0.6.1/dbbench_0.6.1_darwin_amd64.tar.gz"
+  version "0.6.1"
+  sha256 "5a8182514c029fcd7b6c66fc1e8fff9a6ba7c5c1275e5fcfde649286f0b6552b"
 
   def install
     bin.install "dbbench"
