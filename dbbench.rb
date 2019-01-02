baa4d64d262cc72d8c52c77694ce9093a65258f2
goreleaserbot <goreleaser@carlosbecker.com>
2019-01-02 13:06:06 +0100

    Brew formula update for dbbench version v0.6.2

--- dbbench.rb
+++ dbbench.rb
@@ -1,9 +1,9 @@
 class Dbbench < Formula
   desc "dbbench is a simple database benchmarking tool which supports several databases"
   homepage ""
-  url "https://github.com/sj14/dbbench/releases/download/v0.6.1/dbbench_0.6.1_darwin_amd64.tar.gz"
-  version "0.6.1"
-  sha256 "5a8182514c029fcd7b6c66fc1e8fff9a6ba7c5c1275e5fcfde649286f0b6552b"
+  url "https://github.com/sj14/dbbench/releases/download/v0.6.2/dbbench_0.6.2_darwin_amd64.tar.gz"
+  version "0.6.2"
+  sha256 "a673672abcfb381421e7860750fd7bc9c9425032dcc5f18e06a264f715026537"
 
   def install
     bin.install "dbbench"
