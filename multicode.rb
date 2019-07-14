0f4d784f028c8656d6d9bf51acbe2c65875a2955
goreleaserbot <goreleaser@carlosbecker.com>
2019-07-14 10:26:52 +0200

    Brew formula update for multicode version v0.1.2

--- multicode.rb
+++ multicode.rb
@@ -2,17 +2,17 @@
 class Multicode < Formula
   desc "Decode hex, base64 and protobuf recursively with a single command"
   homepage ""
-  version "0.1.1"
+  version "0.1.2"
 
   if OS.mac?
-    url "https://github.com/sj14/multicode/releases/download/v0.1.1/multicode_0.1.1_darwin_amd64.tar.gz"
-    sha256 "051ca07e8f9629864a540cd891960309b89c05a0d1d18721fa4f61e3d22bafd3"
+    url "https://github.com/sj14/multicode/releases/download/v0.1.2/multicode_0.1.2_darwin_amd64.tar.gz"
+    sha256 "feb5fb21cc4fa6e3309d98789b0bdca0ba8dd3df9a597fdb5b8278ae523651e0"
   elsif OS.linux?
-    url "https://github.com/sj14/multicode/releases/download/v0.1.1/multicode_0.1.1_linux_amd64.tar.gz"
-    sha256 "7345ab4c3c607f0ebf9d02cb8c3edd32271366adf1dcbde42fac731deb641c98"
+    url "https://github.com/sj14/multicode/releases/download/v0.1.2/multicode_0.1.2_linux_amd64.tar.gz"
+    sha256 "6c38d79897faad3c051388a7d71538b482bc981688e7448740d9202896c853cc"
   end
 
   def install
-    bin.install "multicode"
+    bin.install "decode"
   end
 end
