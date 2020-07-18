75a2ff3c321ace7d5c4aa9fde56a70f2dffd1076
goreleaserbot <goreleaser@carlosbecker.com>
2020-07-18 19:19:13 +0200

    Brew formula update for epoch version v3.0.2

--- epoch.rb
+++ epoch.rb
@@ -2,24 +2,24 @@
 class Epoch < Formula
   desc "Easily convert epoch timestamps to human readable formats and vice versa."
   homepage ""
-  version "3.0.1"
+  version "3.0.2"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/sj14/epoch/releases/download/v3.0.1/epoch_3.0.1_darwin_amd64.tar.gz"
-    sha256 "7a1b1a84910e303b9013dc523b06fd07f03b77dca9379c90abb869fded123b43"
+    url "https://github.com/sj14/epoch/releases/download/v3.0.2/epoch_3.0.2_darwin_amd64.tar.gz"
+    sha256 "cfb17e234abb5e43971887f478b5c093b9ea70434295805e1a93fe22944826bd"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/sj14/epoch/releases/download/v3.0.1/epoch_3.0.1_linux_amd64.tar.gz"
-      sha256 "81afb861805c109eb65219c25ba7d755d9f59dd9442eeee5d6272ec3cadc9a22"
+      url "https://github.com/sj14/epoch/releases/download/v3.0.2/epoch_3.0.2_linux_amd64.tar.gz"
+      sha256 "037129fad90f51680e88bf0b9baef7a014e207f75b599e8da1d90c381f64d97b"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/sj14/epoch/releases/download/v3.0.1/epoch_3.0.1_linux_arm64.tar.gz"
-        sha256 "9def1803aeb40dc3e8795470030310988a30b530f492548a6b4e98339c68604f"
+        url "https://github.com/sj14/epoch/releases/download/v3.0.2/epoch_3.0.2_linux_arm64.tar.gz"
+        sha256 "44aa9738bbdb248057fe4ecdfbb06ee187e31ece9437fd45235b9becf755726d"
       else
-        url "https://github.com/sj14/epoch/releases/download/v3.0.1/epoch_3.0.1_linux_armv6.tar.gz"
-        sha256 "a0b1af781616b4ee8df506a9d2aedcd5776276a4c1f9b8d56ce411437f9182f5"
+        url "https://github.com/sj14/epoch/releases/download/v3.0.2/epoch_3.0.2_linux_armv6.tar.gz"
+        sha256 "7188838222cb3c3ed71f77d242b8b4a177d6eb7cc48fa5738020575b13cbd718"
       end
     end
   end
